0489f22339452fae364bc41516fa651bcc751393
darcar <daria.nesterovich@duke.edu>
2013-12-04 23:41:48 -0500

    Final Working versions of Health Myelinated Axon
    Includes .m file for analysis

---
+++ Vm_AtNode_ALLTime.m
@@ -0,0 +1,22 @@
+% Kitty Plot
+
+clear;clf
+
+C = textread('Vm_AtONENode_OverALLTime.txt');
+mat_size = size(C);
+space = linspace(0,2680,mat_size(2)-1);
+
+figure(1)
+clf
+
+suptitle('Spatial plots of Vm at node 11 (center node, under electrode)')
+for i=2:mat_size(1)-1
+    subplot(5,2,i-1)
+    plot(space,C(i,1:305),'LineWidth',2)
+    fixplot()
+    str{i} = sprintf('t = %0.2f',((i-2)*50)*0.025);
+    title(str{i})
+    xlabel('Length of Peripheral Process, µm')
+    ylabel('Vm, mV')
+    axis([0 2680 min(min(C(2:end,:))) max(max(C(2:end,:)))])
+end

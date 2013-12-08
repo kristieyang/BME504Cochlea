a74a0f05abd5403f92d511a277b855257a4d5b43
darcar <daria.nesterovich@duke.edu>
2013-12-07 21:50:54 -0500

    Working Activation Plot Code

--- ActivationPlots.m
+++ ActivationPlots.m
@@ -4,21 +4,20 @@
 clear; clc;
 thresh =0; %mV
 currvals = [-0.03]; %mA
-DataLoc = '/winhomes/men14/BME504/ProjectData';
-cd(DataLoc)
+DataLoc = '~/Documents/BME\ 504/Cochlea';
 %cd(DataLoc)
 APmycheck = 0;
 APuncheck = 0;
 
 % Myl File Loop
 
-myfilelist=dir('VmM_Node20_Matrix*');
+myfilelist=dir('VmM_Matrix*');
 for mycount = 1:length(myfilelist);
 eval(['load ' myfilelist(mycount).name ' -ascii']);
-eval(['DataM = VmM_Node20_Matrix' num2str(mycount) ';']);
+eval(['DataM = VmM_Matrix' num2str(mycount) ';']);
 
     for fiber = 1: 100
-        vm = Data(fiber, :);
+        vm = DataM(fiber, :);
         VmMax = max(vm);
             if VmMax >  thresh
                 APmycheck(fiber, mycount) = 1;
@@ -33,13 +32,13 @@ end
 
 clearvars vm Data VmMax
 
-unfilelist = dir('VmU_Node20_Matrix*');
+unfilelist = dir('VmU_Matrix*');
 for uncount = 1:length(unfilelist)
 eval(['load ' unfilelist(uncount).name ' -ascii']);
-eval(['DataU = VmU_Node20_Matrix' num2str(uncount) ';']);
+eval(['DataU = VmU_Matrix' num2str(uncount) ';']);
 
     for fiber = 1: 100
-        vm = Data(fiber, :);
+        vm = DataU(fiber, :);
         VmMax = max(vm);
             if VmMax >  thresh
                 APuncheck(fiber, uncount) = 1;
@@ -50,15 +49,17 @@ eval(['DataU = VmU_Node20_Matrix' num2str(uncount) ';']);
    num_unfiber_AP(uncount,1) = sum(APuncheck)
 end
 
+%num_total_AP = num_unfiber_AP(:)+num_myfiber_AP(:);
+
 for i = 1:length(num_unfiber_AP)
-num_total_AP(i) = num_unfiber_AP(i)+num_myfiber_AP(i);
+    num_total_AP(i) = num_unfiber_AP(i)+num_myfibers_AP(i);
 end
 
 % Plot
 
-p = polyfit(currvals,num_fibers_AP,4);
-fit = polyval(p, linspace(0, 0.15));
+p = polyfit(currvals,num_total_AP,4);
+fit = polyval(p, linspace(-.35,0));
 
 figure(1); clf
 title('Activation % vs. Cathodic Stimulus Amplitude'); xlabel('Cathodic Stimulus Amplitude'); ylabel('Activation Percentage');
-plot(currvals, num_total_AP, 'ro', linspace(0,0.15),fit,'b-')
+plot(currvals, num_total_AP, 'ro', linspace(-0.35,0),fit,'b-')
